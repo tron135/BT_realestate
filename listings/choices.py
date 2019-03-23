9bcb0a5f786307522387350d402f99ebe24d5bad
tron135 <myash180@gmail.com>
2019-03-23 17:46:48 +0530

    search page

---
+++ listings/choices.py
@@ -0,0 +1,43 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10':10
+}
+
+price_choices = {
+    '100000':'$100,000',
+    '200000':'$200,000',
+    '300000':'$300,000',
+    '400000':'$400,000',
+    '500000':'$500,000',
+    '600000':'$600,000',
+    '700000':'$700,000'
+}
+
+state_choices = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AS': 'American Samoa',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'Distric of Columbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    "GU": 'Guan',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana'
+}
